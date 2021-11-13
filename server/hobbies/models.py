ce811f39c669677d0cdb2ded0cf2d2cd9e0262c0
Zak Korme <zakkorme@Zaks-MacBook-Pro.local>
2021-11-13 11:17:08 -0500

    updated hobbies model

--- server/hobbies/models.py
+++ server/hobbies/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 class Hobbies(models.Model):
     hobby_title = models.CharField(max_length=80)
-    hobby_description = models.CharField(max_length=80)
+    hobby_description = models.CharField(max_length=10000)
+    hobby_group = models.CharField(max_length=80)
     number_of_members = models.IntegerField()
